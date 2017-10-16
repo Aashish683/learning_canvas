74d116f02a3378e32535cd4942facbfe232fa975
Aashish Singh <aashishsingh2907@gmail.com>
2017-10-16 10:21:59 +0530

    added function for fps and modified speed to depend on volume

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 var canvas=document.querySelector('canvas');
 var ctx=canvas.getContext('2d');
+var audio=document.querySelector('audio');
+var counter=0;
+audio.volume=0.0;
+var x=audio.volume;
+
+audio.onclick= ()=>{
+    console.log(1);
+};
+/*var timer=setInterval(()=>{
+    while(1)
+    {
+        audio.volume=audio.volume+1/10;
+        if(audio.volume>=0.90)
+        clearInterval(timer);
+    }
+    },3000);*/
 
 
 function Circle(i,j)
@@ -11,8 +27,8 @@ function Circle(i,j)
     this.endAngle=2*Math.PI;
     this.sense=true;
     this.color=`rgba(${Math.floor(255*Math.random())},${Math.floor(255*Math.random())},${Math.floor(255*Math.random())},${Math.random()}`;
-    //this.speed=j==i?10*j+10:2*j+2;
-    this.speed=1*j+2;
+    //this.speed=j==i?10*j+10:2*j+2;;;;
+    this.speed=((audio.volume)*1.5+1)*j;
 }
 
 ctx.translate(500,500);
@@ -27,30 +43,40 @@ ctx.translate(500,500);
 //Moving a circle.
 var start=new Date();
 var seconds=start.getSeconds();
-var milli=start.getMilliseconds();
-draw();
+var milli=Date.now();
+//var milli=start.getMilliseconds();
+var fpsInterval,then,elapsed,now;
+animateFPS(1000);
 //var speed1=14+10*Math.random();
 //draw(2,10,speed1); This function was not executing since window.requestAnimateFrame keeps recurisively calling draw,and that's where the script
-// goes on executing!
+// goes on executing
+
 
 function draw()
 {       
+   now=Date.now();
+   elapsed=now-then;
+   if(elapsed>fpsInterval)
+{
     ctx.save();
     ctx.translate(-500,-500);
     ctx.clearRect(0,0,1000,1000);
     ctx.restore();
     ctx.save();
     var timer=new Date();
+
     
     for(var index=0;index<30;index++)
     {
-        for(var jindex=0;jindex<20;jindex++)
+        for(var jindex=1;jindex<21;jindex++)
         {
             ctx.save();
             ctx.beginPath();
             var c=new Circle(index,jindex);
             ctx.fillStyle=c.color;
-            ctx.rotate(`${c.speed*(2*Math.PI*(timer.getSeconds()-seconds)/60 +2*Math.PI*(timer.getMilliseconds()-milli)/60000)}`);
+            //ctx.rotate(`${c.speed*(2*Math.PI*(timer.getSeconds()-seconds)/60 +2*Math.PI*(timer.getMilliseconds()-milli)/60000)}`);
+   
+            ctx.rotate(`${c.speed*(2*Math.PI*(Date.now()-milli)/60000)}`);
             ctx.translate(c.x,c.y);
             ctx.arc(0,0,c.radius,0,2*Math.PI,true);
             ctx.fill();
@@ -62,9 +88,25 @@ function draw()
 
         }
     }
+    then = now;    
+    counter++;
+    //console.log(counter++);
+    //console.log(audio.volume);
+}
     
     
     
     window.requestAnimationFrame(()=>{draw();});
+    //console.log(audio.volume);
    // setInterval(()=>{draw(i,j,speed);},0.00000000000000000001);
 }
+function animateFPS(fps)
+{
+    fpsInterval=1000/fps;
+    then=Date.now();
+    draw();
+}
+audio.onclick=function(e)
+{
+    console.log(e);
+};
